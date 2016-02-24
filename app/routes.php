f71103d0ffbd8557dce2094d13cbf108c05d5863
stein <stein96@live.nl>
2016-02-24 19:21:33 +0100

    updated example

--- app/routes.php
+++ app/routes.php
@@ -4,4 +4,12 @@ $router->add('/', 'GET', function() {
 	return '<h2>Greetings</h2>';
 });
 
+$router->add('/docs/{*:url}', 'GET', function($url) {
+	return $url;
+});
+
+$router->add('/hello/{a:firstname}/{?:lastname}', 'GET', function($firstname, $lastname = '') {
+	return 'hello '.$firstname.' '.$lastname;
+});
+
 $router->add('/user/{a:id}/edit', 'GET|POST', 'App\Controllers\UserController::show');
