8d257730345df3510e034704744518bb62da3185
Aston Ecole <aston@example.com>
2016-12-01 10:52:58 +0100

    Amélioration du système de template.

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,35 @@
 <?php
 
-$router->add('/', 'GET', function() {
+$router->add('/', 'GET', function () {
 
-    return '<h2>Greetings</h2>';
-	// return $twig->render('home.html.twig');
+    $loader = new Twig_Loader_Filesystem('../templates');
+
+    $twig = new Twig_Environment($loader, array(
+        // 'cache' => '/path/to/compilation_cache',
+        'debug' => true,
+    ));
+
+    return $twig->render('home.html.twig');
 });
 
-$router->add('/docs/{*:url}', 'GET', function($url) {
-	return $url;
+$router->add('/book/add', 'GET', function() {
+
+    $loader = new Twig_Loader_Filesystem('../templates');
+
+    $twig = new Twig_Environment($loader, array(
+        // 'cache' => '/path/to/compilation_cache',
+        'debug' => true,
+    ));
+
+    return $twig->render('book_form.html.twig');
 });
 
-$router->add('/hello/{a:firstname}/{?:lastname}', 'GET', function($firstname, $lastname = '') {
-	return 'hello '.$firstname.' '.$lastname;
-});
-
-$router->add('/user/{a:id}/edit', 'GET|POST', 'App\Controllers\UserController::show');
+// $router->add('/docs/{*:url}', 'GET', function($url) {
+// 	return $url;
+// });
+//
+// $router->add('/hello/{a:firstname}/{?:lastname}', 'GET', function($firstname, $lastname = '') {
+// 	return 'hello '.$firstname.' '.$lastname;
+// });
+//
+// $router->add('/user/{a:id}/edit', 'GET|POST', 'App\Controllers\UserController::show');
--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 use Szenis\RouteResolver;
 use Szenis\Router;
 
+// Sytème d'autoloadiang de composer.
 require __DIR__.'/../vendor/autoload.php';
 
-//  Système de routing.
+// Système de routing.
 require __DIR__.'/../app/router.php';
